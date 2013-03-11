f83bf3559b3a24aa568627b3538608b11fdc273b
Xingxu Wu <pku1001210910@163.com>
2013-03-11 15:10:58 +0800

    add annotations

--- tested_files/type_inference/config/data_detained/DataTaintedConfig.java
+++ tested_files/type_inference/config/data_detained/DataTaintedConfig.java
@@ -1,5 +1,8 @@
 package type_inference.data_detained;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+
 @interface DataType {
 	enum Level {
 		tainted, untainted
@@ -7,7 +10,9 @@ package type_inference.data_detained;
 	Level level();
 }
 
+@DataType
 class DatabaseAccessor {
+	@DataType
 	public String getCriticalData(
 	@DataType(level = DataType.Level.untainted) String sql) {
 		return "Security data";
