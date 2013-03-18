8423fd80d11a4508e6cefa9303c351c109743c6d
Xingxu Wu <pku1001210910@163.com>
2013-03-18 22:10:32 +0800

    delete a line

--- src/core/common/cfg/javacfg/JavaBranchNode.java
+++ src/core/common/cfg/javacfg/JavaBranchNode.java
@@ -3,7 +3,6 @@ package core.common.cfg.javacfg;
 import core.common.cfg.model.BranchNode;
 
 
-
 public class JavaBranchNode extends BranchNode {
 	JavaBranchNode() {
 		super("JavaBranchNode Label");
