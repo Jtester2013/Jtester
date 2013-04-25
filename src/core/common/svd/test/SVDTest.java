b37384ba4bdc60ee1b56bbbe93a51f3a71f7edce
John Chang <zhangzijun1984@gmail.com>
2013-04-25 08:24:32 +0800

    add test package into core.common.svd

---
+++ src/core/common/svd/test/SVDTest.java
@@ -0,0 +1,19 @@
+package test;
+
+public class SVDTest {
+	public int max(int a, int b) {
+		int result = 0;
+		if (a > b) {
+			result = a;
+			result = result + 5;
+		}else {
+			result = b;
+		}
+
+		while (result > 0) {
+			result--;
+			System.out.println();
+		}
+		return result;
+	}
+}
---
+++ src/core/common/svd/test/TestEclipsePaths.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.eclipse.core.runtime.Platform;
+
+public class TestEclipsePaths {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Platform.getInstallLocation());
+	}
+
+}
